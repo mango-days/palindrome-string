356453deeb0e1a2898b1c6139cc35db20bb58aa0
mango-days <positivejasminekaur@gmail.com>
2021-10-28 19:56:17 +0530

    first commit

---
+++ palindrome_string.py
@@ -0,0 +1,10 @@
+def isPalindrome(str, index):
+  middle = int(len(str)/2)
+  if index<=middle:
+    if str[index]==str[-(index+1)]:
+      return isPalindrome(str, index+1)
+    else: print("not a palindrome")
+  else: print("palindrome")
+
+str=input()
+isPalindrome(str)
